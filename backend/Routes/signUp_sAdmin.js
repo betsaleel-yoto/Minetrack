1e836c1374728f6f0c6371cd68ba4928b17f4a18
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-11 11:28:34 +0200

    exportation reussi

--- backend/Routes/signUp_sAdmin.js
+++ backend/Routes/signUp_sAdmin.js
@@ -2,7 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 
-router.get()
-router.post()
-router.delete()
-router.put()
+router.get('',(req,res)=>{
+
+})
+router.post('',(req,res)=>{
+  
+})
+router.delete('',(req,res)=>{
+  
+})
+router.put('',(req,res)=>{
+  
+})
+
+module.exports= router
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port= 3000;
 const sAdminLogin=require('./Routes/signUp_sAdmin')
+
+app.use(express.json())
+app.use(express.urlencoded({extende:true}))
 app.use('/sAdminLogin',sAdminLogin)
 app.listen(port,()=>{
   console.log('le serveur a démaré au port' + port);
