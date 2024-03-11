e2b56f584c3509af234ffcc76fe8bf7b714258ba
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-11 11:45:44 +0200

    route materials

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const sAdminSignup = require("./Routes/signUp_sAdmin");
 const Login = require("./Routes/Login");
 const Users = require("./Routes/users");
 const Vehicle = require("./Routes/vehicle");
+const Materials = require("./Routes/materials");
 // route
 
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use("/sAdminSignup", sAdminSignup);
 app.use("/login", Login);
 app.use("/users", Users);
 app.use("/vehicle", Vehicle);
+app.use("/materials", Materials);
 app.listen(port, () => {
   console.log("le serveur a démaré au port" + port);
 });
