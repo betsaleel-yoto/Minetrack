4b083490aac7f32af6138ad7272e436faa4cae72
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-11 09:20:03 +0200

    fin du frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
-import AddMaterials from "./Pages/AddMaterials";
+import CreateS_admin from "./Pages/CreateS_admin";
+import CreateShipment from "./Pages/CreateShipment";
 import Dashboard from "./Pages/Dashboard";
+import EditS_admin from "./Pages/EditS_admin";
 import ManageOfOrders from "./Pages/ManageOfOrders";
 import ReportsAnalysis from "./Pages/ReportsAnalysis";
+import RoleAuth from "./Pages/RoleAuth";
+import RoutePlanning from "./Pages/RoutePlanning";
+import S_adminLogin from "./Pages/S_adminLogin";
 import SupplierManage from "./Pages/SupplierManage";
+import UserLogin from "./Pages/UserLogin";
 import VehicleManage from "./Pages/VehicleManage";
-import EnteteRapports from "./component/EnTeteRapports";
-import SubTitleReports from "./component/SubTitleReports";
+
 function App() {
   return (
     <>
-<ManageOfOrders/>
     </>
   );
 }
