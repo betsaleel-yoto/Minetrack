119c60f28d42a9d1d2b806b95c48fcc54fb0dfab
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-11 11:35:21 +0200

    correction de lendpoint

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const port= 3000;
-const sAdminLogin=require('./Routes/signUp_sAdmin')
+const sAdminSignup=require('./Routes/signUp_sAdmin')
 
 app.use(express.json())
 app.use(express.urlencoded({extende:true}))
-app.use('/sAdminLogin',sAdminLogin)
+app.use('/sAdminSignup',sAdminSignup)
+app.use('/sAdminLogin',sAdminSignup)
 app.listen(port,()=>{
   console.log('le serveur a démaré au port' + port);
 })
