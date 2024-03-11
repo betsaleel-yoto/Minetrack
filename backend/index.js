0745bb1008f6f55e9d84ff440e890c52bf69f8ad
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-11 11:07:31 +0200

    commencement des routes 1.signup s.admin liaison avec app.use

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const port= 3000;
-
+const sAdminLogin=require('./Routes/signUp_sAdmin')
+app.use('/sAdminLogin',sAdminLogin)
 app.listen(port,()=>{
   console.log('le serveur a démaré au port' + port);
 })
