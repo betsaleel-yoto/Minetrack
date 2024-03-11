1263ac3b57cc873f920c842be8add9d99cf90740
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-11 17:43:46 +0200

    fin de la validation pour la page UserLogin

--- frontend/src/Pages/UserLogin.jsx
+++ frontend/src/Pages/UserLogin.jsx
@@ -1,8 +1,19 @@
 import ConnectForm from "../component/connectForm";
+import validator from 'validator';
 function UserLogin() {
+  const textValidator = (e) => {
+    const inputValue = e.target.value;
+    if (!validator.isLength(inputValue, { min: 1 })) {
+        console.log('le champ ne doit pas être vide');
+    } else if (!validator.matches(inputValue, /^[^<>\s]+$/)) {
+        console.log("ces caractères ne sont pas autorisés");
+    } else {
+        console.log('valide');
+    }
+};
   return ( 
     <>
-<ConnectForm button='Log in'/>
+<ConnectForm button='Log in' change={textValidator}/>
     </>
    );
 }
