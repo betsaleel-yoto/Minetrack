94db96073dac27d53d6d3df0feb59745621cda94
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-11 11:41:02 +0200

    route users

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-const port= 3000;
-const sAdminSignup=require('./Routes/signUp_sAdmin')
+const port = 3000;
+const sAdminSignup = require("./Routes/signUp_sAdmin");
+const Login = require("./Routes/Login");
+const Users = require("./Routes/users");
+// route
 
-app.use(express.json())
-app.use(express.urlencoded({extende:true}))
-app.use('/sAdminSignup',sAdminSignup)
-app.use('/sAdminLogin',sAdminSignup)
-app.listen(port,()=>{
-  console.log('le serveur a démaré au port' + port);
-})
+app.use(express.json());
+app.use(express.urlencoded({ extende: true }));
+app.use("/sAdminSignup", sAdminSignup);
+app.use("/login", Login);
+app.use("/users", Users);
+app.listen(port, () => {
+  console.log("le serveur a démaré au port" + port);
+});
