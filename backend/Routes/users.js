4c6f34fee2e8e172d41fe16523e74e5ecedd0bac
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-13 12:02:40 +0200

    modification des routes Users et materials

--- backend/Routes/users.js
+++ backend/Routes/users.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 
 router.get('',(req,res)=>{
-res.send('bonsoir users')
+
 })
-router.post('',(req,res)=>{
-  
+router.post('/Login',(req,res)=>{
+  res.send('Salut User')
 })
 router.delete('',(req,res)=>{
   
