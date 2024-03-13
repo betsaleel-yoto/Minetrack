300d0771b43002f98ed90c5735b6fca0f52fb956
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-13 21:21:47 +0200

    route edit Sadmin terminée

--- backend/Models/sAdminModel.js
+++ backend/Models/sAdminModel.js
@@ -13,18 +13,22 @@ const getAll =  async(req,res)=>{
 
 const edit = async (req, res) => {
   try {
-    const { newMatriculationNumber, newUsername } = req.body;
+    const { username } = req.body;
     const { matriculationNumber } = req.params; // Récupérer le matriculationNumber des paramètres de la requête
-    const updatedSuperAdmin = await prisma.superAdmin.update({
-      where: {
-        matriculationNumber: matriculationNumber
-      },
-      data: {
-        matriculationNumber: newMatriculationNumber,
-        username: newUsername
-      }
-    });
-    return res.status(201).json(updatedSuperAdmin);
+    await prisma.$transaction(async(prisma)=>{
+      const updatedSuperAdmin = await prisma.SuperAdmin.update({
+        where: {
+          matriculationNumber: matriculationNumber
+        },
+        data: {
+          matriculationNumber,
+          username
+        }
+      });
+      console.log('sAdmin updated:',updatedSuperAdmin)
+      return res.status(201).json(updatedSuperAdmin);
+    })
+
   } catch (error) {
     console.error('Erreur lors de la mise à jour du SuperAdmin :', error);
     return res.status(500).json({ error: 'Erreur lors de la mise à jour du SuperAdmin' });
