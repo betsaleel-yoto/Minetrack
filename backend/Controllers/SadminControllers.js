fb6f538cc59d77d2b9bcf1324ee401090a5216da
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-13 17:15:19 +0200

    Ajout de certains fichiers dans le controller pour plus de bonne pratique

---
+++ backend/Controllers/SadminControllers.js
@@ -0,0 +1,68 @@
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const initializePassport = require('../authenticationFunction/passport-jwt-strategy');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+initializePassport(passport);
+
+const sAdminSignup= async (req,res) => {
+  const {matriculationNumber,username}=req.body
+  try {
+    const superAdmin = await prisma.superAdmin.create({
+      data: {
+        matriculationNumber,
+        username
+      },
+    });
+    console.log('SuperAdmin créé :', superAdmin);
+    
+    // Générer le JWT avec le matricule du superAdmin
+    const token = jwt.sign({
+      matricule: superAdmin.matriculationNumber
+    }, 'zehfgueurfyerfieuyfui', { expiresIn: '1h' });
+    
+    // Renvoyer le token dans la réponse
+    return res.status(201).json({ token });
+  } catch (error) {
+    console.error('Erreur lors de la création du SuperAdmin :', error);
+    res.status(500).json({ error: 'Erreur lors de la création du SuperAdmin' })
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+const sAdminLogin=async (req, res) => {
+  try {
+    // Récupérer le token depuis le corps de la requête ou l'en-tête d'autorisation
+    const token = req.body.token || req.headers.authorization?.split(' ')[1];
+    
+    if (!token) {
+      return res.status(401).json({ message: 'No token provided' });
+    }
+
+    // Décoder le token pour obtenir le matricule
+    const decoded = jwt.verify(token, 'zehfgueurfyerfieuyfui');
+    const matricule = decoded.matricule;
+
+    // Rechercher le matricule dans la base de données
+    const superAdmin = await prisma.superAdmin.findUnique({
+      where: {
+        matriculationNumber: matricule
+      }
+    });
+
+    if (superAdmin) {
+      return res.json({ message: 'Oui, il existe' });
+    } else {
+      return res.json({ message: 'Non, il n\'existe pas' });
+    }
+  } catch (error) {
+    console.error('Erreur lors de la vérification du token :', error);
+    return res.status(401).json({ message: 'Token verification failed' });
+  }
+}
+
+module.exports={
+sAdminSignup,sAdminLogin
+}
--- backend/Routes/S_admin.js
+++ backend/Routes/S_admin.js
@@ -1,73 +1,15 @@
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const initializePassport = require('../authenticationFunction/passport-jwt-strategy');
 const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const {sAdminSignup,sAdminLogin}=require('../Controllers/SadminControllers')
 
-initializePassport(passport)
 
 router.get('',(req,res)=>{
   
 })
 
-router.post('/Signup', async (req,res) => {
-  const {matriculationNumber,username}=req.body
-  try {
-    const superAdmin = await prisma.superAdmin.create({
-      data: {
-        matriculationNumber,
-        username
-      },
-    });
-    console.log('SuperAdmin créé :', superAdmin);
-    
-    // Générer le JWT avec le matricule du superAdmin
-    const token = jwt.sign({
-      matricule: superAdmin.matriculationNumber
-    }, 'zehfgueurfyerfieuyfui', { expiresIn: '1h' });
-    
-    // Renvoyer le token dans la réponse
-    return res.status(201).json({ token });
-  } catch (error) {
-    console.error('Erreur lors de la création du SuperAdmin :', error);
-    res.status(500).json({ error: 'Erreur lors de la création du SuperAdmin' })
-  } finally {
-    await prisma.$disconnect();
-  }
-})
+router.post('/Signup',sAdminSignup)
 
-router.post('/Login', async (req, res) => {
-  try {
-    // Récupérer le token depuis le corps de la requête ou l'en-tête d'autorisation
-    const token = req.body.token || req.headers.authorization?.split(' ')[1];
-    
-    if (!token) {
-      return res.status(401).json({ message: 'No token provided' });
-    }
-
-    // Décoder le token pour obtenir le matricule
-    const decoded = jwt.verify(token, 'zehfgueurfyerfieuyfui');
-    const matricule = decoded.matricule;
-
-    // Rechercher le matricule dans la base de données
-    const superAdmin = await prisma.superAdmin.findUnique({
-      where: {
-        matriculationNumber: matricule
-      }
-    });
-
-    if (superAdmin) {
-      return res.json({ message: 'Oui, il existe' });
-    } else {
-      return res.json({ message: 'Non, il n\'existe pas' });
-    }
-  } catch (error) {
-    console.error('Erreur lors de la vérification du token :', error);
-    return res.status(401).json({ message: 'Token verification failed' });
-  }
-});
+router.post('/Login', sAdminLogin);
 
 
 router.put('',(req,res)=>{
