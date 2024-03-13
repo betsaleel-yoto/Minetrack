778617d03826ac8a2e99552ddd6e5e301abe8875
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-13 22:37:41 +0200

    ajout fonction edit user

--- backend/Models/UserModel.js
+++ backend/Models/UserModel.js
@@ -11,6 +11,32 @@ const getAll =  async(req,res)=>{
   }
 }
 
+const edit = async (req, res) => {
+  try {
+    const { matriculationNumber,UserName,UserRole,UserTitle } = req.body;
+    const { matriculation } = req.params; // Récupérer le matriculationNumber des paramètres de la requête
+    await prisma.$transaction(async(prisma)=>{
+      const updatedUser = await prisma.User.update({
+        where: {
+          matriculationNumber: matriculation
+        },
+        data: {
+          matriculationNumber,
+          UserName,
+          UserRole,
+          UserTitle
+        }
+      });
+      console.log('sAdmin updated:',updatedUser)
+      return res.status(201).json(updatedUser);
+    })
+
+  } catch (error) {
+    console.error('Erreur lors de la mise à jour du SuperAdmin :', error);
+    return res.status(500).json({ error: 'Erreur lors de la mise à jour du SuperAdmin' });
+  }
+}
+
 module.exports={
-  getAll
+  getAll,edit
 }
--- backend/Routes/users.js
+++ backend/Routes/users.js
@@ -2,7 +2,7 @@ const {UserSignup,UserLogin} = require('../Controllers/UserControllers')
 const express = require("express");
 const router = express.Router();
 
-const {getAll}=require('../Models/UserModel')
+const {getAll,edit}=require('../Models/UserModel')
 
 router.get('/getAll',getAll);
 
@@ -10,9 +10,7 @@ router.post('/Signup',UserSignup );
 
 router.post('/Login',UserLogin );
 
-router.put('/edit',(req,res)=>{
-
-});
+router.put('/edit/:matriculation',edit);
 
 router.delete('/delete',(req,res)=>{
 
