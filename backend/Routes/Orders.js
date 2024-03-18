6d8a668e8157c7261c0c303685b492d94a10bcb1
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-18 12:46:10 +0200

    version des routes Orders

--- backend/Routes/Orders.js
+++ backend/Routes/Orders.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getAllOrders,
+  editOrder,
+  deleteOrder,
+  addOrder
+}=require('../Models/ordersModel')
 
-
-router.get('',(req,res)=>{
-
-})
-router.post('/Create',(req,res)=>{
-  res.send('bonsoir commandes')
-})
-router.delete('',(req,res)=>{
-  
-})
-router.put('',(req,res)=>{
-  
-})
+router.get('/getAll',getAllOrders)
+router.post('/Add',addOrder)
+router.delete('/delete/:id',deleteOrder)
+router.put('/edit/:id',editOrder)
 
 module.exports= router
