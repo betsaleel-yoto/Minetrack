c6e36012d5bcc466d387fcf569ee1e1fa243db08
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-20 11:42:01 +0200

    appel SupplierManage ok

--- frontend/src/Pages/SupplierManage.jsx
+++ frontend/src/Pages/SupplierManage.jsx
@@ -1,35 +1,114 @@
 import EnteteTableau from "../component/EnteteTableau";
 import validator from 'validator';
+import { useState } from "react";
 import NavBar from "../component/navBar";
 import Input from "../component/inputs/input";
 import DoubleButton from "../component/Button/DoubleBoutton";
 import LineTableu from "../component/LineTableau";
-import SuperTitle from "../component/SuperTitle";
-import IconsEditDelete from "../component/IconsEditDelete";
 import ElementTableau1 from "../component/ElementTableau1";
-import UniqueButton from "../component/Button/UniqueButton";
-import Select from "../component/inputs/Select";
 function SupplierManage() {
-  const textValidator = (e) => {
-    const inputValue = e.target.value;
-    if (!validator.isLength(inputValue, { min: 1 })) {
-        console.log('le champ ne doit pas être vide');
-    } else if (!validator.matches(inputValue, /^[^<>\s]+$/)) {
-        console.log("ces caractères ne sont pas autorisés");
-    } else {
-        console.log('valide');
-    }
-};
 
-const dateValidator = (e) => {
-  const inputValue = e.target.value;
-  const regexDate = /^\d{4}-\d{2}-\d{2}$/;
-  if (regexDate.test(inputValue)) {
-      console.log("La date est valide.");
-  } else {
-      console.log("La date n'est pas valide.");
+const [MaterialName,setMaterialName]=useState('')
+const [DateOf_Order,setDateOf_Order]=useState('')
+const [Quantity,setQuantity]=useState('')
+const [DeliveryDate,setDeliveryDate]=useState('')
+
+const textValidator = (inputValueA, inputValueB) => {
+  try {
+    if (!validator.isLength(inputValueA, { min: 1 })) {
+      console.log('le champ A ne doit pas être vide');
+    } else if (!validator.matches(inputValueA, /^[^<>]+$/)) {
+      console.log("ces caractères ne sont pas autorisés pour le champ A");
+    } else if (!validator.isLength(inputValueB, { min: 1 })) {
+      console.log('le champ B ne doit pas être vide');
+    } else if (!validator.matches(inputValueB, /^[^<>]+$/)) {
+      console.log("ces caractères ne sont pas autorisés pour le champ B");
+    } else {
+      console.log('valide');
+    }
+  } catch (error) {
+    console.error('Une erreur est survenue lors de la validation :', error);
   }
 };
+
+const dateValidator = (inputValue) => {
+  try {
+    const regexDate = /^\d{4}-\d{2}-\d{2}$/;
+    if (regexDate.test(inputValue)) {
+     console.log("La date est valide.");
+    } else {
+      console.log("La date n'est pas valide.");
+    }
+  } catch (error) {
+    console.error('Une erreur est survenue lors de la validation de la date :', error);
+    console.log('Une erreur est survenue lors de la validation de la date');
+  }
+};
+
+const sendData = () => {
+  const requestData = {
+    MaterialName: MaterialName ,
+		DateOf_Order:DateOf_Order,
+		Quantity:Quantity,
+		DeliveryDate:DeliveryDate,
+    matriculationNumberSadmin: "jshfvdfrfreefrfejvjdfvjdfsdfsfdcsd"
+  };
+
+  // Effectuer la requête POST en utilisant fetch
+  fetch('http://localhost:3000/orders/Add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestData)
+  })
+    .then(response => {
+      // Vérifiez si la réponse est ok
+      if (!response.ok) {
+        throw new Error('Erreur lors de la requête');
+      }
+      // Si la réponse est ok, retournez les données en JSON
+      return response.json();
+    })
+    .then(data => {
+      // Récupérer l'ID et l'InitialQte du matériau ajouté
+      const OrderId = data.id;
+
+      // Stocker les données dans le localStorage
+      localStorage.setItem('MaterialID', OrderId);
+      console.log('commande faite ajouté');
+      
+      // Vous pouvez effectuer d'autres actions avec les données de la réponse si nécessaire
+    })
+    .catch(error => {
+      // Gérer les erreurs éventuelles
+      console.error('Erreur lors de la requête :', error);
+    });
+};
+
+const handleMaterialName = (e) => {
+  setMaterialName(e.target.value); // Mettre à jour l'état matriculationNumber avec la valeur entrée
+  textValidator(e.target.value, MaterialName); // Appel de textValidator avec la nouvelle valeur du matriculationNumber
+};
+
+const handleQuantity = (e) => {
+  setQuantity(e.target.value); // Mettre à jour l'état matriculationNumber avec la valeur entrée
+  textValidator(e.target.value, MaterialName); // Appel de textValidator avec la nouvelle valeur du matriculationNumber
+};
+
+
+const handleDateOf_Order = (e) => {
+  setDateOf_Order(e.target.value); // Mettre à jour l'état matriculationNumber avec la valeur entrée
+  dateValidator(e.target.value); // Appel de textValidator avec la nouvelle valeur du matriculationNumber
+};
+
+const handleDeliveryDate = (e) => {
+  setDeliveryDate(e.target.value); // Mettre à jour l'état matriculationNumber avec la valeur entrée
+  dateValidator(e.target.value); // Appel de textValidator avec la nouvelle valeur du matriculationNumber
+};
+
+
+
   return ( 
     <>
     <div className="flex w-[100%]">
@@ -47,7 +126,7 @@ const dateValidator = (e) => {
                 name="MaterialName"
                 label="Material Name"
                 htmlFor="Materialname"
-                change={textValidator}
+                change={handleMaterialName}
               />
 
 
@@ -57,7 +136,7 @@ const dateValidator = (e) => {
                 name="DateOf_Order"
                 label="Date of order"
                 htmlFor="DateOfOrder"
-                change={dateValidator}
+                change={handleDateOf_Order}
               />
 
               <Input
@@ -66,7 +145,7 @@ const dateValidator = (e) => {
                 name="Quantity"
                 label="Quantity"
                 htmlFor="Quantity"
-                change={textValidator}
+                change={handleQuantity}
               />
 <Input
                 classes="w-[100%]"
@@ -74,10 +153,10 @@ const dateValidator = (e) => {
                 name="DeliveryDate"
                 label="Determine a delivery date"
                 htmlFor="DeliveryDate"
-                change={dateValidator}
+                change={handleDeliveryDate}
               />
 
-              <DoubleButton />
+              <DoubleButton click={sendData}/>
             </form>
           </div>
 
