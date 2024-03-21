ecfd7956abc014101e1fadd35ea543c5077a0130
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-21 16:42:06 +0200

    reajustement check

--- frontend/src/component/FormTask.jsx
+++ frontend/src/component/FormTask.jsx
@@ -1,14 +1,22 @@
+import React, { useState } from "react";
+
 function FormTask(props) {
+  const [isChecked, setIsChecked] = useState(props.checked);
+
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked);
+    props.change(event); // Appel de la fonction de gestion de la case à cocher du composant parent
+  };
+
   return (
     <>
       <form action="" method="POST" className="pt-4 pl-5">
         <input
           type="checkbox"
           name="taskFinished"
-          id=""
-          checked={props.checked}
+          checked={isChecked}
           className="font-semibold font-raleway"
-          onChange={props.change}
+          onChange={handleCheckboxChange}
         />{" "}
         <label
           htmlFor="checkbox"
