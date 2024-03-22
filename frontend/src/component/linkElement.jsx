755ba437f68dc48ccd8335735b5a4334f3117666
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-22 18:07:58 +0200

    un ajout fin

--- frontend/src/component/linkElement.jsx
+++ frontend/src/component/linkElement.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { NavLink  } from "react-router-dom";
 function LinkElement(props) {
   return ( 
     <>
     <div className="pt-5 border-b border-white h-11 pb-14  hover:bg-[#4886FF]">
-      <Link to={props.link} className="flex" onClick={props.click}>
+      <NavLink to={props.link } className="flex" onClick={props.click}>
       <img src={props.src} alt="" className="px-8"/>
-      <p className="relative font-semibold text-white font-raleway left-5 w-96">{props.titleLink}</p>
+      <p className="relative font-semibold text-white font-raleway left-5 w-96">{props.titleLink }</p>
       <img src="/src/img/🦆 icon _nav arrow right_.svg" alt="" className="px-11"/>
-      </Link>
+      </NavLink >
     </div>
     </>
    );
