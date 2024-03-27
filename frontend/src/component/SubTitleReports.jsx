6686ecb0550b5334b2090d59527bee1ada3d3134
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-27 13:46:10 +0200

    affichage report

--- frontend/src/component/SubTitleReports.jsx
+++ frontend/src/component/SubTitleReports.jsx
@@ -1,9 +1,9 @@
-function SubTitleReports() {
+function SubTitleReports(props) {
   return ( 
     <>
     <div className=" w-[80%] pt-[1rem] pl-[1rem] pb-3 m-auto flex">
-<p className="font-semibold font-raleway text-[#5D5D5D]">Monitor environmental impact and implement mitigation measures</p>
-<p  className="pl-24 font-semibold font-raleway text-[#5D5D5D]">Finished:11/02/2025</p>
+<p className="font-semibold font-raleway text-[#5D5D5D]">{props.text}</p>
+<p  className="pl-24 font-semibold font-raleway text-[#5D5D5D]">Finished:{props.Date}</p>
 <img src="/src/img/fleche_bas.svg" alt="" className="pl-[20%]"/>
 
     </div>
