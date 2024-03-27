a55c3818bd8c549260972494e842d4f21cacf8d2
betsaleel-yoto <betsaleelyoto@gmail.com>
2024-03-27 13:59:43 +0200

    affichage ordersManage

--- frontend/src/Pages/ManageOfOrders.jsx
+++ frontend/src/Pages/ManageOfOrders.jsx
@@ -1,7 +1,28 @@
 import EnteteRapports from "../component/EnTeteRapports";
 import NormalTitle from "../component/NormalTitle";
 import SuperLineBlue from "../component/SuperlineBlue";
+import { useState,useEffect } from "react";
 function ManageOfOrders() {
+const [Orders,setOrders]=useState([])
+
+  useEffect(() => {
+
+    fetch('http://localhost:3000/orders/getAll')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erreur lors de la récupération des données');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Données récupérées avec succès
+        setOrders(data)
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des données :', error);
+      });
+  }, []);
+
   return ( 
     <>
     <div className="h-[20%] bg-[#39527B] flex">
@@ -12,9 +33,15 @@ function ManageOfOrders() {
     <div className="w-[100%] h-auto pt-6">
 <EnteteRapports text='List of orders'/>
 <SuperLineBlue text1='Order ID' text2='Date' text3='Product' text4='Quantity'/>
-<NormalTitle text1='1' text2='2024-02-27' text3='Product A' text4='90'/>
-<NormalTitle text1='2'text2='2024-02-27' text3='Product B' text4='5'/>
-<NormalTitle text1='3' text2='2024-02-27' text3='Product C' text4='1 500 000 $'/>
+{Orders.map(order=>(
+  <NormalTitle
+  key={order.id}
+ text1={order.id}
+  text2={order.DeliveryDate}
+  text3={order.MaterialName}
+  text4={order.Quantity}/>
+))}
+
 </div>
     </>
    );
